32ffad82faa940d752eef66100228173408d1934
2bit-hack <sohamkar76@gmail.com>
2021-06-01 00:37:55 +0530

    Add layout and basic styling

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 Inspired by websites like [Google Fight](https://www.googlefight.com/) and [Google Battle](http://www.googlebattle.com/)
 
-Given two search keywords, this website displays which of the two keywords has more search results on search engines like [Google](https://www.google.com/) and [Bing](https://www.bing.com/)
+Given two search terms, this website displays which of the two keywords has more search results on search engines like [Google](https://www.google.com/) and [Bing](https://www.bing.com/)
--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,20 @@ import './App.css'
 function App() {
   return (
     <div className="App">
-      Hello, Search Wars!
+      <header>
+        <p className="heading">Search Wars!</p>
+      </header>
+      <div className="search-container">
+        <div className="search-row">
+          <p className="label">Search Term 1</p>
+          <input className="search" type="text" />
+        </div>
+        <div className="search-row">
+          <p className="label">Search Term 2</p>
+          <input className="search" type="text" />
+        </div>
+      </div>
+      <button className="submit-btn">FIGHT</button>
     </div>
   )
 }
